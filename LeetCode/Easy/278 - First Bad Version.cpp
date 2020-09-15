02246b578f61b1bc50e773f759d307725aeddca2
Tanjim Bin Faruk <1505082.tbf@ugrad.cse.buet.ac.bd>
2020-09-16 04:51:40 +0600

    LC Update

---
+++ LeetCode/Easy/278 - First Bad Version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 0, high = n;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
